4764b17ad1776b2be2edcbda8d8a961129df9ebb
Thais Souza <75867936+so-tha@users.noreply.github.com>
2022-08-25 16:07:43 -0300

    add logicaI&logicaII

---
+++ logicaI/alert.js
@@ -0,0 +1,2 @@
+//A funcionalidade desta instrução é exibir um popup com o texto que definirmos.
+    alert("Primeira instrução JavaScript que aprendi");
---
+++ logicaI/formula.js
@@ -0,0 +1,5 @@
+
+for(let n = 0; n <= 5; n++){
+    document.write("O resultado da fórmula é : " + n * (10 / 20)  );
+}
+
